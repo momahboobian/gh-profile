949174ff145591dcf9f8207737e5ee12e21357aa
Lorena <lorenacapraru@gmail.com>
2023-07-25 18:14:19 +0100

    Add the github avatar to the db

--- pages/api/gitHubAPI.js
+++ pages/api/gitHubAPI.js
@@ -3,13 +3,28 @@ const octokit = new Octokit({
   auth: process.env.GITHUB_AUTH_TOKEN,
 });
 
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async function handler(req, res) {
   //   const { owner } = req.body;
   const owner = "lorenacapraru";
   try {
     const commits = await octokit.request(`GET /users/${owner}/events`);
-    return res.json({ commits: commits.data });
+
+    const avatar_url = commits.data[0].actor.avatar_url;
+    const user = commits.data[0].actor.login;
+    console.log(user);
+
+    await prisma.graduate.updateMany({
+      where: { github: owner },
+      data: {
+        avatar: { set: avatar_url },
+      },
+    });
+
+    return res.json(commits.data);
   } catch (error) {
     console.error("Error fetching data:", error);
     return res
