91d80c7d7dee67c08db88169a34c20e898b3a478
Mo Mahboobian <momahboobian@gmail.com>
2023-07-27 13:37:03 +0100

    modifiying the syles

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+      <div className="flex w-full max-w-5xl items-center justify-between text-sm lg:flex">
         {profiles.map((profile) => (
           <ProfileCard key={profile.id} profile={profile} />
         ))}
--- components/ProfileCard.js
+++ components/ProfileCard.js
@@ -3,78 +3,47 @@ import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 export default function ProfileCard({ profile }) {
   return (
-    <div className="profile-card bg-neutral-800	rounded-lg shadow-md p-1 w-80">
-      <div className="flex flex-col justify-between items-center bg-black rounded-t-lg p-4">
+    <div className="flex-1 bg-neutral-800	rounded-lg shadow-md p-1 w-80">
+      <div className="flex flex-col justify-between items-center whitespace-nowrap bg-black rounded-t-lg p-4">
         <img
           src={profile.avatar}
           alt={`Avatar of ${profile.fullName}`}
           className="w-28 h-28 rounded-lg mx-auto mb-2"
         />
-        <h2 className="text-lg ">{profile.fullName}</h2>{" "}
+        <h2 className="text-lg font-light">{profile.fullName}</h2>{" "}
       </div>
-      <div className="flex flex-col justify-between items-center p-4">
-        <div className="mt-4 flex flex-col">
-          <p>
-            <span className="font-bold">Role:</span> {profile.role}
-          </p>
-          <p>
-            <span className="font-bold">Cohort:</span> {profile.cohort}
-          </p>
+      <div className="flex flex-col justify-between items-center whitespace-nowrap p-4">
+        <div className="flex justify-between items-center whitespace-nowrap gap-4">
+          <div className="flex-1">
+            <p className="text-center text-base">{profile.role}</p>
+            <div className="text-center text-sm text-[#606467] font-light">
+              Role
+            </div>
+          </div>
+          <div className="flex-1">
+            <p className="text-center text-base">{profile.cohort}</p>
+            <div className="text-center text-sm text-[#606467] font-light">
+              Cohort
+            </div>
+          </div>
         </div>
-        <div className="flex mt-4">
+
+        <div className="flex items-center justify-around">
           <a
             href={profile.github}
             target="_blank"
             rel="noopener noreferrer"
-            className="mr-2 inline-block bg-blue-500 hover:bg-blue-600 text-white px-3 py-1 rounded-lg"
+            className="text-white h-[35px] transition duration-300 hover:scale-110 hover:text-[#37BCBA]"
           >
             <FontAwesomeIcon icon={faGithub} className="mr-1" />
-            GitHub
           </a>
           <a
             href={profile.linkedin}
             target="_blank"
             rel="noopener noreferrer"
-            className="inline-block bg-blue-500 hover:bg-blue-600 text-white px-3 py-1 rounded-lg"
+            className="text-white h-[35px] transition duration-300 hover:scale-110 hover:text-[#37BCBA]"
           >
             <FontAwesomeIcon icon={faLinkedin} className="mr-1" />
-            LinkedIn
-          </a>
-        </div>
-        <div className="mt-4">
-          <a
-            href={profile.cv}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mr-2 inline-block bg-blue-500 hover:bg-blue-600 text-white px-3 py-1 rounded-lg"
-          >
-            CV
-          </a>
-          <a
-            href={profile.coverLetter}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="inline-block bg-blue-500 hover:bg-blue-600 text-white px-3 py-1 rounded-lg"
-          >
-            Cover Letter
-          </a>
-        </div>
-        <div className="mt-4">
-          <a
-            href={profile.gitFP}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mr-2 inline-block bg-blue-500 hover:bg-blue-600 text-white px-3 py-1 rounded-lg"
-          >
-            GitHub FP
-          </a>
-          <a
-            href={profile.demoFP}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="inline-block bg-blue-500 hover:bg-blue-600 text-white px-3 py-1 rounded-lg"
-          >
-            Demo FP
           </a>
         </div>
       </div>
