1f94649f27cc381204d0a91445541b6e3eb9afae
Lorena <lorenacapraru@gmail.com>
2023-09-05 21:17:14 +0100

    Updated states from Your Github Username to Graduate Username

--- components/AddGraduateForm.js
+++ components/AddGraduateForm.js
@@ -2,11 +2,13 @@
 import { useState } from "react";
 
 export default function AddGraduateForm({ data }) {
-  const [fullName, setFullName] = useState("Your Full Name");
-  const [gitHubUsername, setGitHubUsername] = useState("Your Github Username");
-  const [linkedin, setLinkedin] = useState("Your LinkedIn");
-  const [role, setRole] = useState("Your Role");
-  const [cohort, setCohort] = useState("Your Cohort");
+  const [fullName, setFullName] = useState("Graduate Name");
+  const [gitHubUsername, setGitHubUsername] = useState(
+    "Graduate Github Username"
+  );
+  const [linkedin, setLinkedin] = useState("Graduate LinkedIn");
+  const [role, setRole] = useState("Graduate Role");
+  const [cohort, setCohort] = useState("Graduate Cohort");
 
   const handleClickCohort = () => {
     setCohort("");
@@ -27,35 +29,48 @@ export default function AddGraduateForm({ data }) {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const newGraduate = {
-        fullName: fullName,
-        github: gitHubUsername,
-        avatar:
-          "https://raw.githubusercontent.com/1l0/identicon/master/example/identicons/default.png",
-        linkedin: linkedin,
-        role: role,
-        cohort: cohort,
-      };
+      if (
+        fullName &&
+        fullName !== "Graduate Name" &&
+        gitHubUsername &&
+        gitHubUsername !== "Graduate Github Username" &&
+        linkedin &&
+        linkedin !== "Graduate LinkedIn" &&
+        role &&
+        role !== "Graduate Role" &&
+        cohort &&
+        cohort !== "Graduate Cohort"
+      ) {
+        const newGraduate = {
+          fullName: fullName,
+          github: gitHubUsername,
+          avatar:
+            "https://raw.githubusercontent.com/1l0/identicon/master/example/identicons/default.png",
+          linkedin: linkedin,
+          role: role,
+          cohort: cohort,
+        };
 
-      const response = await fetch("/api/addMember", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newGraduate),
-      });
-      const data = await response.json();
-      console.log("add member", data);
-      data.push(newGraduate);
-      if (response.ok) {
-        console.log("Graduate data sent successfully", data);
-        // Clear form fields
-        setFullName("");
-        setGitHubUsername("");
-        setLinkedin("");
-        setCV("");
-        setCohort("");
-        setRole("");
+        const response = await fetch("/api/addMember", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newGraduate),
+        });
+        const data = await response.json();
+        console.log("add member", data);
+        data.push(newGraduate);
+        if (response.ok) {
+          console.log("Graduate data sent successfully", data);
+          // Clear form fields
+          setFullName("");
+          setGitHubUsername("");
+          setLinkedin("");
+          setCV("");
+          setCohort("");
+          setRole("");
+        }
       } else {
         console.error("Failed to send graduate data");
       }
@@ -67,7 +82,7 @@ export default function AddGraduateForm({ data }) {
   return (
     <form
       id="graduateForm"
-      className="absolute rounded-xl align-middle top-[23%] left-[35%]   flex flex-col align-center  z-40 bg-white p-8 text-[#606467] w-[30%] "
+      className="absolute rounded-xl align-middle top-[20%] left-[35%]   flex flex-col align-center  z-40 bg-white p-8 text-[#606467] w-[30%] "
       onSubmit={handleSubmit}
     >
       <legend className="text-xl font-bold	 text-black">Add new Graduate</legend>
@@ -82,6 +97,7 @@ export default function AddGraduateForm({ data }) {
         className="bg-[#f8f8f8]  border-2  border-solid	 p-2 rounded-md mb-7"
         onChange={(e) => setFullName(e.target.value)}
         onClick={handleClickFullName}
+        required
       />
 
       <label htmlFor="githubUsername">GitHub Username:</label>
@@ -93,6 +109,7 @@ export default function AddGraduateForm({ data }) {
         className="bg-[#f8f8f8]  border-2  border-solid	 p-2 rounded-md mb-7"
         onChange={(e) => setGitHubUsername(e.target.value)}
         onClick={handleClickGitHubUsername}
+        required
       />
 
       <label htmlFor="linkedin">LinkedIn:</label>
@@ -104,6 +121,7 @@ export default function AddGraduateForm({ data }) {
         value={linkedin}
         onChange={(e) => setLinkedin(e.target.value)}
         onClick={handleClickLinkedin}
+        required
       />
       <div className=" flex  justify-around ">
         <div>
@@ -116,6 +134,7 @@ export default function AddGraduateForm({ data }) {
             value={role}
             onChange={(e) => setRole(e.target.value)}
             onClick={handleClickRole}
+            required
           />
         </div>
 
@@ -129,6 +148,7 @@ export default function AddGraduateForm({ data }) {
             value={cohort}
             onChange={(e) => setCohort(e.target.value)}
             onClick={handleClickCohort}
+            required
           />
         </div>
       </div>
@@ -138,7 +158,7 @@ export default function AddGraduateForm({ data }) {
       <input
         type="submit"
         value="Submit"
-        className=" p-4 cursor-pointer bg-[#37BCBA] hover:bg-[#1a9997] font-medium rounded-md  my-[10%] text-center text-md text-white"
+        className=" p-2 cursor-pointer bg-[#37BCBA] hover:bg-[#1a9997] font-medium rounded-md  my-[5%] text-center text-md text-white"
       />
     </form>
   );
